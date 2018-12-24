a2a06f3f5ec4dbbf3ef0baead06c67d9fedb31e8
nguyenvubichvan1991 <44745546+nguyenvubichvan1991@users.noreply.github.com>
2018-12-24 15:04:08 +0700

    Update truonghoc.php

--- truonghoc.php
+++ truonghoc.php
@@ -1,75 +1,10 @@
-﻿<?php
-include_once "Database.php";
-
-if(!isset($_POST['btnthem']) || $_POST['btnthem']=="")
-{
-    $_POST['txttentruong']= "";
-    $_POST['txtuser']="";
-    $_POST['txtcontent']="";
-    
-    //$rows= array();
-    $db=new DataBase();
-    $query="select * from schools";
-    $rows = $db->exec_select($query);
-}
-
-if(isset($_POST['btnthem']))
-{
-    $db = new Database();
-    $names=$_POST['txttentruong'];
-    $user=$_POST['txtuser'];
-    $nd=$_POST['txtcontent'];
-    $sql = "insert into schools(Names, Username, Manl) values('$names','$user','$nd')";
-	$db->exec_insert($sql);
-    $sql2="select * from schools";
-    $rows = $db->exec_select($sql2);
-}
-?>
-
-<form name="bd_page" method="post">
-<h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-THÊM THÔNG TIN TRƯỜNG HỌC</h2>
-<b>Tên trường: <input type="text" name="txttentruong" size="12" />&nbsp;&nbsp;&nbsp;
-UserName: <input type="text" name="txtuser" size="20" />&nbsp;&nbsp;&nbsp; 
-Content: <input type="text" name="txtcontent" size="12" />&nbsp;&nbsp;&nbsp;  
-</b><br /><br />
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<input type="submit" name="btnthem" value="Thêm" /> &nbsp;&nbsp;&nbsp; 
-
-
-<h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-DANH SÁCH TRƯỜNG HỌC</h3>
-<table style="border: 1px solid;">
-    <tr>
-        <td style="border: 1px solid; width: 30px; text-align: center; background: #D8D8D8; font-style: oblique;"><strong>STT</strong> </td>
-        <td style="border: 1px solid; width: 200px; text-align: center; background: #D8D8D8; font-style: oblique;"><strong>Tên trường</strong> </td>
-        <td style="border: 1px solid; width: 210px; text-align: center; background: #D8D8D8; font-style: oblique;"><strong>UserName</strong></td>
-        <td style="border: 1px solid; width: 145px; text-align: center; background: #D8D8D8; font-style: oblique;"><strong>Content</strong> </td>
-   </tr>
-    <?php
-        $stt=0;
-        foreach($rows as $r)
-        {
-    ?>
-    <tr>
-        <td style="border: 1px solid;"> <?= ++$stt ?></td>
-        <td style="border: 1px solid; text-align: center;"> <?= $r['Names'] ?> </td>
-        <td style="border: 1px solid;"> <?= $r['Username'] ?></td>
-        <td style="border: 1px solid; text-align: center;"> <?= $r['Manl'] ?></td>
-    </tr>
-    
-    <?php 
-        }
-    ?>
-</table>
-<?php
-    echo "Tổng cộng: ".$stt; 
-?>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="dangnhap.php">Trở về trang chủ <a/> &nbsp;&nbsp;&nbsp; 
-
+ <form action="result.php"> 
+				  <select name="truonghoc" onchange="showtruonghoc(this.value)">
+					<option value="">danh sách trường học:</option>
+					<option value="khtn">Chuyên Hùng Vương BD</option>
+					<option value="khxh ">Nguyễn Khuyến BD</option>
+					<option value="tkm">Chuyên Lê Quý Đôn</option>
+					<option value="kts">Chuyên Đại Học Bách Khoa TPHCM</option>
+					<option value="nt">Chuyên Văn hóa Nghệ thuật</option>	
+				  </select>
 </form>
