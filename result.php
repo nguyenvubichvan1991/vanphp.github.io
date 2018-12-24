116ed1f963a5a755a85c243e3335eeb55dd9f482
nguyenvubichvan1991 <44745546+nguyenvubichvan1991@users.noreply.github.com>
2018-12-24 16:15:28 +0700

    Update result.php

--- result.php
+++ result.php
@@ -3,7 +3,6 @@
 <head>
 	<meta http-equiv="content-type" content="text/html" charset="utf-8" />
 	<meta name="author" content="GallerySoft.info" />
-
 	<title>HỆ THỐNG GỢI Ý TRƯỜNG HỌC THEO NĂNG LỰC, SỞ THÍCH</title>
     <link rel="stylesheet" type="text/css" href="StyleSheet.css" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -14,21 +13,15 @@
 		  });
 		});
 		</script>
-		
 </head>
-
-    
 <body>
     <div id="top">
-        <a href="#" style="text-decoration: none; background-color:yellow; font-size:24px"><BR/><BR/>HỆ THỐNG GỢI Ý TRƯỜNG HỌC THEO NĂNG LỰC, SỞ THÍCH</a>
+        <a href="#" style="text-decoration: none; background-color:yellow; font-size:24px"><BR/>HỆ THỐNG GỢI Ý TRƯỜNG HỌC THEO NĂNG LỰC, SỞ THÍCH</a>
     </div>
     <div id="menu">
         <ul>
-             <li style="border-left: none;">
-                <a href="index.php">Trang chủ</a>
-            </li>
+             <li style="border-left: none;"><a href="index.php">Trang chủ</a></li>
             <li><a href="Login.php">Đăng nhập</a></li>
-            
         </ul>
     </div>
     <div id="wrapper">
@@ -38,10 +31,10 @@
 			<p><a href="truonghoc.php" id="w3s">Chúc mừng bạn đã chọn ngành học phù hợp với năng lực</a></p>
 		</form>	
             </div> 
-            <div id="right">Quang cao</div>
+            <div id="right"></div>
     </div>
     <div id="bottom">
-        Copyright &copy; 2018 by Nguyen Vu Bich Van
+        Copyright &copy; 2018 by DLB16HT201
     </div>
 </body>
 </html>
