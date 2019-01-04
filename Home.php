f91e888da7848e7f8b040afeab573504ce24624d
nguyenvubichvan1991 <44745546+nguyenvubichvan1991@users.noreply.github.com>
2019-01-05 04:07:31 +0700

    Update Home.php

--- Home.php
+++ Home.php
@@ -3,7 +3,6 @@
 <head>
 	<meta http-equiv="content-type" content="text/html" charset="utf-8" />
 	<meta name="author" content="GallerySoft.info" />
-
 	<title>HỆ THỐNG GỢI Ý TRƯỜNG HỌC THEO NĂNG LỰC, SỞ THÍCH</title>
     <link rel="stylesheet" type="text/css" href="StyleSheet.css" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -13,68 +12,41 @@
 			alert($("#w3s").attr("href"));
 		  });
 		});
-		</script>
-		
+		</script>	
 </head>
-
-    
 <body>
     <div id="top">
-        <a href="#" style="text-decoration: none; background-color:yellow; font-size:24px"><BR/><BR/>HỆ THỐNG GỢI Ý TRƯỜNG HỌC THEO NĂNG LỰC, SỞ THÍCH</a>
+        <a href="#" style="text-decoration: none; background-color:yellow; font-size:24px">
+		<BR/><BR/>HỆ THỐNG GỢI Ý TRƯỜNG HỌC THEO NĂNG LỰC, SỞ THÍCH</a>
     </div>
-    <div id="menu">
-        <ul>
-             <li style="border-left: none;">
-                <a href="index.html">Trang chủ</a>
-            </li>
-            <li><a href="#"> Danh mục</a></li>
-            <li><a href="#">Tra cứu</a></li>
-            <li><a href="Login.html">Đăng nhập</a></li>
-            
-        </ul>
+    <div id="menungang">
+		<nav >
+                    <a href="#" id="menu-icon"></a>
+                    <ul>
+                        <li  style="border-left: none;"><a href="index.php">TRANG CHỦ</a>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+                        <a href="nangluc.php">Tư vấn lựa chọn trường</a>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp
+                        <a href="thongke.php">Thống kê dữ liệu</a>
+			</li>
+                    </ul>
+                </nav>
     </div>
     <div id="wrapper">
-            <div id="left">Left menu
-            </div>
+            <div id="left"></div>
             <div id="main">
-			   <p><a href="result.html" id="w3s">Vui lòng chọn năng lực phù hợp</a></p>
-			   <form action="result.html"> 
-				  <select name="nangluc" onchange="shownangluc(this.value)">
-					<option value="">Chọn năng lực:</option>
-					<option value="khtn">Khoa học tự nhiên</option>
-					<option value="khxh ">Khoa học xã hội</option>
-					<option value="tkm">Thiết kế mẫu</option>
-					<option value="kts">Kiến trúc sư</option>
-					<option value="nt">Nghệ thuật</option>
-					
-				  </select>
-				  <button>Xác nhận</button>
-				</form>
-				
+		<p></p>
             </div> 
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            <div id="right">Quang cao</div>
+            <div id="right"></div>
     </div>
     <div id="bottom">
-        Copyright &copy; 2018 by Nguyen Vu Bich Van
+        Copyright &copy; 2018 by Nguyễn Vũ Bích Vân
     </div>
 </body>
 </html>
