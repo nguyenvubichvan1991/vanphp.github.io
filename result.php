eb0803ce66d4390044df888717498847603936eb
nguyenvubichvan1991 <44745546+nguyenvubichvan1991@users.noreply.github.com>
2019-01-17 23:15:25 +0700

    Update result.php

--- result.php
+++ result.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>HỆ THỐNG QUẢN LÝ TRƯỜNG HỌC </title>
+    <title>HỆ THỐNG LỰA CHỌN TRƯỜNG HỌC THEO NĂNG LỰC</title>
     <link href="css/StyleSheet.css" rel="stylesheet" type="text/css" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script>
